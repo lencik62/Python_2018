c850e4bbe768b48b8a0bb6924acfd8dd124b82cd
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-05-25 17:27:27 -0400

    selenium & scrapy used to test and scrape data from a site

---
+++ Working/Scraping/Independent/BookGrabber/BookGrabber/spiders/sel_spider_multi_page.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.http import Request
+from time import sleep
+from selenium.common.exceptions import  NoSuchElementException
+class sel_spider_multi_page(Spider):
+    name = 'sel'
+    allowed_domains = ['books.toscrape.com']
+
+    def start_requests(self):
+        self.driver = driver = webdriver.Chrome("chromedriver")
+        self.driver.get("http://books.toscrape.com/")
+        
+        sel = Selector(text = self.driver.page_source)
+        books = sel.xpath('//h3/a/@href').extract()
+        for book in books:
+            url = "http://books.toscrape.com/" + book
+
+            yield Request (url , callback = self.parse_book)
+        while True:
+            try:
+                self.driver.find_element_by_xpath('//a[text() ="next"]').click()
+                sleep(3)
+                self.logger.info('Sleeping for 3 seconds')
+                sel = Selector(text = self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+                for book in books:
+                    url = "http://books.toscrape.com/catalogue/" + book
+
+                    yield Request (url , callback = self.parse_book)
+            except NoSuchElementException:
+                self.logger.info("No more pages to load")
+
+                break
+    def parse_book(self, response):
+
+        pass
