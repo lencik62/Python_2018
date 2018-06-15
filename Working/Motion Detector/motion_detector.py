06d428cd3bd0145b6ea5175c60a865b7b7f21995
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-06-14 20:15:15 -0400

    prettify and add text on motion statues on screen

--- Working/Motion Detector/motion_detector.py
+++ Working/Motion Detector/motion_detector.py
@@ -1,64 +1,65 @@
 import cv2, time, pandas
 from datetime import datetime
 
-first_frame=None
-status_list=[None,None]
-times=[]
-df=pandas.DataFrame(columns=["Start","End"])
+first_frame = None
+status_list = [None, None]
+times = []
+df = pandas.DataFrame(columns = ["Start", "End"])
 
-video=cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-    status=0
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray=cv2.GaussianBlur(gray,(21,21),0)
+    status = 0
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     if first_frame is None:
-        first_frame=gray
+        first_frame = gray
         continue
 
-    delta_frame=cv2.absdiff(first_frame,gray)
-    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+    delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
-    (_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (_, cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
-        status=1
+        status = 1
 
-        (x, y, w, h)=cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
     status_list.append(status)
 
-    status_list=status_list[-2:]
+    status_list = status_list[-2:]
 
 
-    if status_list[-1]==1 and status_list[-2]==0:
+    if status_list[-1] == 1 and status_list[-2] == 0:
         times.append(datetime.now())
-    if status_list[-1]==0 and status_list[-2]==1:
+    if status_list[-1] == 0 and status_list[-2] == 1:
         times.append(datetime.now())
 
+    cv2.putText(frame, "Motion Detected? : {}".format("Yes" if status == 1 else "NO"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-    cv2.imshow("Gray Frame",gray)
-    cv2.imshow("Delta Frame",delta_frame)
-    cv2.imshow("Threshold Frame",thresh_frame)
-    cv2.imshow("Color Frame",frame)
+    cv2.imshow("Gray Frame", gray)
+    cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
-    key=cv2.waitKey(1)
+    key = cv2.waitKey(1)
 
-    if key==ord('q'):
-        if status==1:
+    if key == ord('q'):
+        if status == 1:
             times.append(datetime.now())
         break
 
 print(status_list)
 print(times)
 
-for i in range(0,len(times),2):
-    df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+for i in range(0, len(times), 2):
+    df = df.append({"Start":times[i], "End":times[i+1]}, ignore_index = True)
 
 df.to_csv("Times.csv")
 
--- Working/Motion Detector/plotter.py
+++ Working/Motion Detector/plotter.py
@@ -3,21 +3,21 @@ from bokeh.plotting import figure, show, output_file
 from bokeh.models import HoverTool, ColumnDataSource
 from os import startfile
 from subprocess import call
-df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
-df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
-cds=ColumnDataSource(df)
+cds = ColumnDataSource(df)
 
-p=figure(x_axis_type='datetime',height=100, width=500, responsive=True,title="Motion Graph")
-p.yaxis.minor_tick_line_color=None
-p.ygrid[0].ticker.desired_num_ticks=1
+p = figure(x_axis_type = 'datetime', height = 100, width = 500, responsive = True, title = "Motion Graph")
+p.yaxis.minor_tick_line_color = None
+p.ygrid[0].ticker.desired_num_ticks = 1
 
-hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+hover = HoverTool(tooltips = [("Start", "@Start_string"), ("End", "@End_string")])
 p.add_tools(hover)
 
-q=p.quad(left="Start",right="End",bottom=0,top=1,color="green",source=cds)
-file_name ="Graph1.html"
+q = p.quad(left = "Start", right = "End", bottom = 0, top = 1, color = "green", source = cds)
+file_name = "Graph1.html"
 output_file(file_name)
 show(p)
 
