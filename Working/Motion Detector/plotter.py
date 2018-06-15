2224cee7eba2e2ada519b5be560480ff995c1dbf
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-06-14 20:00:51 -0400

    auto open the file

--- Working/Motion Detector/plotter.py
+++ Working/Motion Detector/plotter.py
@@ -1,7 +1,8 @@
 from motion_detector import df
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import HoverTool, ColumnDataSource
-
+from os import startfile
+from subprocess import call
 df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
 df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -16,6 +17,15 @@ hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
 p.add_tools(hover)
 
 q=p.quad(left="Start",right="End",bottom=0,top=1,color="green",source=cds)
-
-output_file("Graph1.html")
+file_name ="Graph1.html"
+output_file(file_name)
 show(p)
+
+
+
+def click_on_file(filename):
+    '''Open document with default application in Python.'''
+    try:
+        startfile(filename)
+    except AttributeError:
+        call(['open', filename])
