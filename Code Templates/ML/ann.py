6ea1741a43f293ac7b4b5536c279fc2349ff892f
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-07-14 20:27:23 -0400

    cleaning up classigier

--- Code Templates/ML/ann.py
+++ Code Templates/ML/ann.py
@@ -3,11 +3,16 @@ from tensorflow.keras.layers import Dense, Dropout
 def build_classifier():
     classifier = Sequential()
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
-    # dropout is to reduce the chances of overfitting {p= .1-.4}
-    classifier.add(Dropout(rate=.1))
+    # dropout is to reduce the chances of overfitting {p = .1-.4}
+    classifier.add(Dropout(rate = .1))
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dropout(rate=.1))
+    classifier.add(Dropout(rate = .1))
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-    classifier.add(Dropout(rate=.1))
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
+
+def main():
+    build_classifier()
+
+if __name__ == '__main__':
+    main()
---
+++ Code Templates/ML/cnn.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+    classifier.add(MaxPooling2D(pool_size = (2, 2)))
+    classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+    classifier.add(MaxPooling2D(pool_size = (2, 2)))
+    classifier.add(Flatten())
+    classifier.add(Dense(units = 128, activation = 'relu'))
+    classifier.add(Dense(units = 1, activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+def main():
+    build_classifier()
+
+if __name__ == '__main__':
+    main()
